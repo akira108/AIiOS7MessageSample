a2699d0554be99ceba93b04cc7e72d0d83af673d
Akira Iwaya <tamago.panda@gmail.com>
2013-12-14 00:51:34 +0900

    Initial Commit

---
+++ AICollectionViewWithDynamics/AIViewController.h
@@ -0,0 +1,13 @@
+//
+//  AIViewController.h
+//  AICollectionViewWithDynamics
+//
+//  Created by Akira Iwaya on 2013/12/14.
+//  Copyright (c) 2013年 akira108. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AIViewController : UIViewController
+
+@end
---
+++ AICollectionViewWithDynamics/AIViewController.m
@@ -0,0 +1,29 @@
+//
+//  AIViewController.m
+//  AICollectionViewWithDynamics
+//
+//  Created by Akira Iwaya on 2013/12/14.
+//  Copyright (c) 2013年 akira108. All rights reserved.
+//
+
+#import "AIViewController.h"
+
+@interface AIViewController ()
+
+@end
+
+@implementation AIViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
